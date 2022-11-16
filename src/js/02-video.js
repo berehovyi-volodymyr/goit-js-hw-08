3833cb96197656f79101a0128d798c2a3e801729
berehovyi-volodymyr <106471456+berehovyi-volodymyr@users.noreply.github.com>
2022-11-16 22:06:47 +0200

    done 2 tasks

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Player from '@vimeo/player';
+const throttle = require('lodash.throttle');
+
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+const LOCALSTORAGE_KEY = "videoplayer-current-time";
+
+player.on('timeupdate', throttle(catchSecond, 1000))
+
+function catchSecond ({seconds}) {
+    localStorage.setItem(LOCALSTORAGE_KEY, seconds)
+};
+
+const time = localStorage.getItem(LOCALSTORAGE_KEY)
+
+player.setCurrentTime(time)
+
 
