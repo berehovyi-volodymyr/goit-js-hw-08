1ed892c5c643919b60eecaf33c0ea8013551be8d
berehovyi-volodymyr <106471456+berehovyi-volodymyr@users.noreply.github.com>
2022-11-16 17:40:21 +0200

    1 task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-console.log(galleryItems);
+const ref = {
+  gallery: document.querySelector(".gallery"),
+};
+
+const galleryMarkup = creatElementsMarkup();
+
+function creatElementsMarkup() {
+  return galleryItems
+    .map(
+      ({ original, preview, description }) =>
+        `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" loading="lazy" src="${preview}" alt="${description}" title="${description}" />
+</a>`
+    )
+      .join("");
+}
+
+ref.gallery.insertAdjacentHTML("beforeend", galleryMarkup);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captions: true,
+    captionDelay: 250,
+});
