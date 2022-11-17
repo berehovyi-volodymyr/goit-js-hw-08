32abd5a56c7f83e33d8f6fc763a35e758b7e9761
berehovyi-volodymyr <106471456+berehovyi-volodymyr@users.noreply.github.com>
2022-11-17 13:43:00 +0200

    done 3 tasks

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,39 @@
+const throttle = require('lodash.throttle');
 
+const ref = {
+  form: document.querySelector('.feedback-form'),
+  email: document.querySelector('input[type="email"]'),
+  message: document.querySelector('textarea'),
+};
+
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+const formData = {};
+
+ref.form.addEventListener('input', throttle(onInputs, 500));
+ref.form.addEventListener('submit', onButton);
+
+reload();
+
+function onInputs(e) {
+  formData[e.target.name] = e.target.value;
+  const saveFormdata = JSON.stringify(formData);
+  localStorage.setItem(LOCALSTORAGE_KEY, saveFormdata);
+}
+
+function onButton(e) {
+    e.preventDefault();
+    e.target.reset();
+    const result = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY))
+    console.log(result)
+    localStorage.removeItem(LOCALSTORAGE_KEY);
+}
+
+function reload() {
+  const savedMessageJSON = localStorage.getItem(LOCALSTORAGE_KEY);
+  if (savedMessageJSON) {
+    const savedMessage = JSON.parse(savedMessageJSON);
+
+    ref.email.value = savedMessage.email;
+    ref.message.value = savedMessage.message;
+  }
+}
